3e127745b09f6cd6791165fa675e37d72995d7ad
parxdoxe <dumoulin.florian22@gmail.com>
2022-09-27 08:33:46 +0200

    initial project

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+
+        
+       @yield('content')
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/produits', [ProductController::class, 'index'])->name('products');
+
+Route::get('/mon-panier', [CartController::class, 'index'])->name('cart');
+Route::get('/ajouter-a-mon-panier/{id}', [CartController::class, 'addToCart'])->name('add.to.cart');
+Route::get('/supprimer-de-mon-panier/{id}', [CartController::class, 'remove'])->name('remove.to.cart');
+
+Route::get('/connexion', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/connexion', [LoginController::class, 'store']);
+Route::get('/deconnexion', [LoginController::class, 'destroy'])->name('logout')->middleware('auth');
+
+Route::get('/inscription', [RegisterController::class, 'index'])->name('register');
+Route::post('/inscription', [RegisterController::class, 'store']);
